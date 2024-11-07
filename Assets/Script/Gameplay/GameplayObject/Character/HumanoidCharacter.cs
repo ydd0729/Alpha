97029e46b64396f619db4f6142025ee65cbadc59
Yang Dong <yangdong_gamedeveloper@outlook.com>
2024-11-07 18:34:28 +0800

    Humanoid Character & Remove Character Data

---
+++ Assets/Script/Gameplay/GameplayObject/Character/HumanoidCharacter.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Yd.Gameplay.Object
+{
+    public class HumanoidCharacter : Character
+    {
+        [SerializeField] private GameObject leftFoot;
+        [SerializeField] private GameObject rightFoot;
+
+        [CanBeNull] public GameObject LeftFoot => leftFoot;
+        [CanBeNull] public GameObject RightFoot => rightFoot;
+    }
+}
