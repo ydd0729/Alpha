d29349a66144abcd759f8bbd118b0b3a36b3a95d
Yang Dong <yangdong_gamedeveloper@outlook.com>
2024-11-07 18:27:16 +0800

    Organize Assets & Add ProBuilder

---
+++ Assets/Character/Stylized Human Male V2/Script/Editor/StylizedHumanMaleV2ConfigureButtonEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Yd.StylizedHumanMaleV2
+{
+    [CustomEditor(typeof(StylizedHumanMaleV2ConfigureButton))]
+    public class StylizedHumanMaleV2ConfigureButtonEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Configure"))
+            {
+                ((StylizedHumanMaleV2ConfigureButton)target).gameObject.GetComponent<StylizedHumanMaleV2>().Configure();
+            }
+        }
+    }
+}
---
+++ Assets/Character/Stylized Human Male V2/Script/StylizedHumanMaleV2ConfigureButton.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace Yd.StylizedHumanMaleV2
+{
+    [RequireComponent(typeof(StylizedHumanMaleV2))]
+    public class StylizedHumanMaleV2ConfigureButton : MonoBehaviour
+    {
+    }
+}
+#endif
